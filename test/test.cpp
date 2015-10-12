11d7bd6562cc0a307669a6d31430574aa7538ea0
nabijaczleweli-vm <nabijaczleweli@gmail.com>
2015-10-12 13:47:22 +0200

    Add bandit submodule. Build tests only if bandit is cloned

--- test/test.cpp
+++ test/test.cpp
@@ -20,65 +20,9 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-#include "../include/cpponfig/cpponfig_version.hpp"
-#include "../include/cpponfig/configuration.hpp"
-#include "../include/cpponfig/util/strings.hpp"
-#include <sstream>
-#include <iostream>
-#include <exception>
+#include "bandit/bandit/bandit.h"
 
 
-using namespace std;
-using namespace cpponfig;
-using namespace cpponfig::util;
-
-
-int main() {
-	cout << "Cpponfiguration v. " << cpponfiguration_version << "\n\n";
-
-	configuration::datetime_footer_type = configuration::datetime_mode::gmt;
-	property::floating_precision        = 10;
-
-	configuration cfg;
-	istringstream input_stream(R"TEXT(
-	                           # This is a test
-	                           # SOF comment
-
-	                           { # This is a category
-	                             mo0 = asdf
-	                             asdf=m#00
-	                             moo=asdf # asdf moo
-	                             #moo=asdf
-	                             m0o=[0,1,2,3,4]
-	                             0m0=[false,true,false,false,true]
-	                             m00=[1.25,3.7,233.7,20,1.99090909909]
-	                           }
-	                           )TEXT");
-	try {
-		cfg.load(input_stream);
-		cout << "Loading succeeded!\n";
-	} catch(...) {
-		const char * what = nullptr;
-		const auto exc = current_exception();
-		if(exc)
-			try {
-				rethrow_exception(exc);
-			} catch(const exception & e) {
-				what = e.what();
-			} catch(...) {
-			}
-		cout << "Loading failed!" << (what ? (" What: "s + what + '.') : "") << '\n';
-	}
-	cfg.save(cout);
-
-	cfg.get("0m0").boolean_list().emplace_back(false);
-	cfg.get("m00").floating_list().emplace_back(3.14159);
-	cfg.get("0m0").update_from_boolean_list();
-	cfg.get("m00").update_from_floating_list();
-
-	cout << "\n\"";
-	if(cfg.save(cout))
-		cout << "\"\nSaved properly.\n";
-	else
-		cout << "Didn\'t save.\n";
+int main(int argc, char * argv[]) {
+	return bandit::run(argc, argv);
 }
--- wscript
+++ wscript
@@ -1,22 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 
 from datetime import datetime
+from os import listdir
 
 
 def options(opts):
 	opts.load('compiler_cxx')
-	opts.add_option('--build-test', dest='build_test', default=False, action='store_true', help='build tests')
 
 def configure(conf):
 	conf.load('compiler_cxx')
-	conf.env.append_value('INCLUDES', ['.', '../include/cpponfig'])
+	conf.env.append_value('INCLUDES', ['.', '../include/cpponfig', '../bandit'])
 	conf.check(features='cxx cxxprogram', cxxflags=['-std=c++14', '-Wall', '-Wextra', '-O3', '-pedantic', '-pipe'], uselib_store='M')
 	conf.check(features='cxx cxxprogram', function_name='std::put_time<char>', header_name='iomanip', use='M', mandatory=False)
 	conf.define('CPPONFIGURATION_VERSION', datetime.utcnow().strftime('%Y.%m.%d %H:%M:%S UTC'))
 	conf.write_config_header('config.h')
 
 def build(buld):
+	print(buld.env)
 	buld(features='cxx cxxstlib', source=buld.path.ant_glob('src/**/*.cpp'), target='cpponfig', use='M')
-	if buld.options.build_test:
-		buld(features='cxx cxxprogram', source=buld.path.ant_glob('test/**/*.cpp'), target='tester', use=['M', 'cpponfig'])
+	if listdir('bandit'):
+		buld(features='cxx cxxprogram', source=buld.path.ant_glob('test/**/*.cpp'), target='cpponfig_test', use=['M', 'cpponfig'])
